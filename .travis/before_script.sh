ddeb976aeb70fb3191bb669a9a855320b8cff759
Jeroen Thora <jeroenthora@gmail.com>
2018-04-28 11:42:30 +0200

    fix

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -34,4 +34,4 @@ bin/console doctrine:schema:create --env=dev --no-interaction
 #bin/console doctrine:schema:update --force --env=test_travis || exit $?
 
 echo "Setting the web assets up"
-bin/console assets:install --env=test_travis --no-debug -vvv || exit $?
+#bin/console assets:install --env=dev --no-debug -vvv || exit $?
